9131dbeb7977a82eaf9fe2934c3779054fb02ada
Tirina Tirikatene <tirinatirikatene@yahoo.com>
2024-12-15 13:55:48 +1300

    add entry functional

--- models/entries.ts
+++ models/entries.ts
@@ -4,12 +4,13 @@ export interface Entry {
   date: string
   location_name: string
   details: string
-  image_url: string | null
+  image_url: string 
 }
 
 export interface EntryData {
   date: string
   location_name: string
   details: string
-  image_url: string | null
+  image_url: string
 }
+
